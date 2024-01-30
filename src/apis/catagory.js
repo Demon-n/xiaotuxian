68c8089de09e6db4e34e76e78234921355853739
Demon-n <2993188146@qq.com>
2024-01-30 11:03:08 +0800

    二级分类-基础列表渲染

--- src/apis/catagory.js
+++ src/apis/catagory.js
@@ -1,8 +1,8 @@
-import httpInstance from "@/utils/http";
+import request from "@/utils/http";
 
 //面包屑导航
 export const getCategoryAPI = (id) => {
-    return httpInstance({
+    return request({
         url: '/category',
         params: {
             id
@@ -16,12 +16,28 @@ export const getCategoryAPI = (id) => {
  */
 
 export const getCategoryFilterAPI = (id) => {
-    return httpInstance({
+    return request({
         url: '/category/sub/filter',
         params: {
             id
         }
     })
 }
-
+/**
+ * @description: 获取导航数据
+ * @data { 
+     categoryId: 1005000 ,
+     page: 1,
+     pageSize: 20,
+     sortField: 'publishTime' | 'orderNum' | 'evaluateNum'
+   } 
+ * @return {*}
+ */
+export const getSubCategoryAPI = (data) => {
+    return request({
+        url: '/category/goods/temporary',
+        method: 'POST',
+        data
+    })
+}
 
--- src/utils/http.js
+++ src/utils/http.js
@@ -1,20 +1,20 @@
 //axios基础的封装
 import axios from 'axios'
 
-const httpInstance = axios.create({
+const request = axios.create({
     baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
     timeout: 5000
 })
 
 
 //请求拦截器
-httpInstance.interceptors.request.use(config => {
+request.interceptors.request.use(config => {
     return config
 }, e => Promise.reject(e))
 
 //响应拦截器
-httpInstance.interceptors.response.use(res => res.data, e => {
+request.interceptors.response.use(res => res.data, e => {
     return Promise.reject(e)
 })
 
-export default httpInstance 
+export default request 

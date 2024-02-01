d5d5e958f60ba50186a36f35f66a8a1c3d1035ef
Demon-n <2993188146@qq.com>
2024-02-01 17:04:51 +0800

    登录-表单校验-自定义校验规则

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -3,7 +3,8 @@ import { ref } from 'vue'
 
 const form = ref({
     account: '',
-    password: ''
+    password: '',
+    agree: true
 })
 const rules = {
     account: [
@@ -12,6 +13,21 @@ const rules = {
     password: [
         { required: 'true', message: '密码不能为空', trigger: 'blur' },
         { min: 6, max: 10, message: '密码长度6-10个字符', trigger: 'blur' }
+    ],
+    agree: [
+        {
+            validator: (rule, value, callback) => {
+                console.log(value)
+                //自定义校验逻辑
+                //勾选就通过 不勾选就不通过
+                if (value) {
+                    callback()
+                }
+                else {
+                    callback(new Error('请勾选协议'))
+                }
+            }
+        }
     ]
 }
 </script>
@@ -45,8 +61,8 @@ const rules = {
                             <el-form-item prop="password" label="密码">
                                 <el-input v-model="form.password" />
                             </el-form-item>
-                            <el-form-item label-width="22px">
-                                <el-checkbox size="large">
+                            <el-form-item prop="agree" label-width="22px">
+                                <el-checkbox v-model="form.agree" size="large">
                                     我已同意隐私条款和服务条款
                                 </el-checkbox>
                             </el-form-item>

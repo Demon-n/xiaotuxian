f40f69439d234f15e4ce0359aac9b8d141fe2790
Demon-n <2993188146@qq.com>
2024-02-14 15:24:38 +0800

    本地购物车-列表购物车-单选功能

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -30,6 +30,13 @@ export const useCartStore = defineStore('cart', () => {
         cartList.value.splice(idx, 1)
     }
 
+    //单选功能
+    const singleCheck = (skuId, selected) => {
+        //通过skuId找到要修改的那一项 然后把它的selected赋值为新传过来的selected
+        const item = cartList.value.find((item) => item.skuId === skuId)
+        item.selected = selected
+    }
+
     //总的数量 所有项的count之和
     const allCount = computed(() => cartList.value.reduce((a, c) => a + c.count, 0))
     const allPrice = computed(() => cartList.value.reduce((a, c) => a + c.count * c.price, 0))
@@ -38,7 +45,8 @@ export const useCartStore = defineStore('cart', () => {
         allCount,
         allPrice,
         addCart,
-        delCart
+        delCart,
+        singleCheck
     }
 },
     {
